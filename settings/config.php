f11d73c268350a7878b61bfcc74cacca80e284fa
Suyash Kawna <srkawna@users.noreply.github.com>
2015-08-10 22:12:09 +0530

    Update config.php

--- settings/config.php
+++ settings/config.php
@@ -3,7 +3,7 @@ $config = array(
 	'username' => 'admin8irhRKl@127.3.76.130',
 	'password' => 'IzzvCxAIEtv3',
 	'database'=>'colosseum',
-	'hostname'=>'127.3.76.130',
+	'hostname'=>'127.3.76.130:3306',
 	'db_driver'=>'mysql'
 
 );
