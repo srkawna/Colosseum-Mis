8f1e629eb8fdb05901f4417187372b3bf1761094
Suyash Kawna <srkawna@users.noreply.github.com>
2015-08-10 21:54:19 +0530

    Update config.php

--- settings/config.php
+++ settings/config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-	'username' => 'admin8irhRKl',
+	'username' => 'admin8irhRKl@127.3.76.130',
 	'password' => 'IzzvCxAIEtv3',
 	'database'=>'colosseum',
 	'hostname'=>'127.3.76.130:3306',
