473e390dfd8c431ab1f2c6ea19e6e04ae049a28d
Suyash Kawna <srkawna@users.noreply.github.com>
2015-08-11 11:10:02 +0530

    Update config.php

--- settings/config.php
+++ settings/config.php
@@ -1,7 +1,7 @@
 <?php
 $config = array(
-	'username' => 'admin8irhRKl',
-	'password' => 'IzzvCxAIEtv3',
+	'username'=>'admin8irhRKl',
+	'password'=>'IzzvCxAIEtv3',
 	'database'=>'colosseum',
 	'hostname'=>'127.3.76.130',
 	'db_driver'=>'mysql'
