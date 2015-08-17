27ca52651507c74087d0f8e827345410b8cbb9b2
Suyash Kawna <srkawna@users.noreply.github.com>
2015-08-17 18:22:35 +0530

    Update config.php

--- settings/config.php
+++ settings/config.php
@@ -1,9 +1,9 @@
 <?php
 $config = array(
-	'username'=>'admin8irhRKl',
-	'password'=>'IzzvCxAIEtv3',
+	'username'=>'admin45YKmHg',
+	'password'=>'Xan2BwP-JgMM',
 	'database'=>'colosseum',
-	'hostname'=>'127.3.76.130:3306',
+	'hostname'=>'mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/',
 	'db_driver'=>'mysql'
 
 );
